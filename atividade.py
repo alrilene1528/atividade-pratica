38e401c1ea7f3beee6f4ef5511789cbe34aaddc9
alrilenemn21 <alrilene1809@gmail.com>
2020-08-22 20:31:08 +0000

    atividade de pratica profissional

---
+++ atividade.py
@@ -0,0 +1,22 @@
+idade = 25
+altura = 1.84
+status = True
+status = False
+
+
+print ('idade' , idade)
+pessoa = ['Lucas Moura', 25,1.84,'Professor']
+print(f'nome', {pessoa[0]})
+print(f'idade', {pessoa[1]})
+print(f'altura', {pessoa[2]})
+print(f'profissão',{pessoa[3]})
+
+
+pessoa = {
+
+    'nome': 'Marcos Barbosa' ,
+    'idade': 30,
+    'peso':68.5,
+
+    }
+print (pessoa)
